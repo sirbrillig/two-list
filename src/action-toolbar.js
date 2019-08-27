33aff15b21f08abb0f16c2badae974749f2f82ed
Payton Swick <payton@foolord.com>
2019-08-27 12:58:35 -0400

    Add missing file

---
+++ src/action-toolbar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import BottomNavigation from '@material-ui/core/BottomNavigation';
+import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
+import Icon from '@material-ui/core/Icon';
+import useStyles from './style';
+
+export default function ActionToolbar({ createNewItem }) {
+  const classes = useStyles();
+  const addIcon = <Icon className={classes.addItemButton}>add_circle</Icon>;
+  return (
+    <BottomNavigation showLabels>
+      <BottomNavigationAction label="Add" icon={addIcon} onClick={createNewItem} />
+    </BottomNavigation>
+  );
+}
