331a2ab731a19f8e8147656b9e5f68d5501dcd16
Payton Swick <payton@foolord.com>
2019-09-11 17:13:12 -0400

    Save trip to localStorage

--- src/logged-in.js
+++ src/logged-in.js
@@ -25,10 +25,43 @@ function useScrollToItem(tripLocations, targetListRef) {
   }, [targetListRef, tripLocations]);
 }
 
+function useLocalStorageState(initialValue, key) {
+  const [state, setState] = useState(initialValue);
+  useEffect(() => {
+    let storedValue;
+    try {
+      storedValue = window.localStorage.getItem(key);
+      if (storedValue) {
+        setState(JSON.parse(storedValue));
+      }
+    } catch (error) {
+      console.log('reading local state failed', error);
+      return;
+    }
+  }, [key]);
+  function setAndSaveState(newValue) {
+    if (typeof newValue === 'function') {
+      newValue = newValue(state);
+    }
+    try {
+      const encodedValue = JSON.stringify(newValue);
+      console.log('saving local state', newValue, 'to key', key);
+      window.localStorage.setItem(key, encodedValue);
+    } catch (error) {
+      console.log('saving local state failed', error);
+    }
+    setState(newValue);
+  }
+  return [state, setAndSaveState];
+}
+
 export default function LoggedIn({ classes, logOut }) {
   const [items, setItems] = useVoyageurSync();
 
-  const [tripLocations, setTripLocations] = useState([]);
+  const [tripLocations, setTripLocations] = useLocalStorageState(
+    [],
+    'voyageurTripLocations',
+  );
   const [itemDetail, showItemDetail] = useState();
   const [isShowingAddItem, setIsShowingAddItem] = useState(false);
   const targetListRef = useRef();
