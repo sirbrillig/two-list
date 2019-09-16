a24c688f673e1a4d10c05a034bb7fa4dcfcfdacb
Payton Swick <payton@foolord.com>
2019-09-16 11:20:00 -0400

    Add example private vars

---
+++ src/example-private-vars.js
@@ -0,0 +1,4 @@
+export const VOYAGEUR_API_SERVER = 'https://voyageur-js.herokuapp.com';
+export const AUTH0_CLIENT_ID = 'SOMETHING HERE';
+export const AUTH0_DOMAIN = 'foolord.auth0.com';
+export const AUTH0_AUDIENCE = 'https://voyageur-js.herokuapp.com/';
