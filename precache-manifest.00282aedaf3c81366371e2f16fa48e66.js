29645b069edac4f6274253d4cb0a37272cf68f1c
Payton Swick <payton@foolord.com>
2019-11-30 14:56:12 -0500

    Updates

---
+++ precache-manifest.00282aedaf3c81366371e2f16fa48e66.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05c974c75f4f1116928b8528696f1374",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f4cbacb65d269e39d194",
+    "url": "/static/css/main.9bfd661b.chunk.css"
+  },
+  {
+    "revision": "37cfff88c82fa826f44e",
+    "url": "/static/js/2.cd96de54.chunk.js"
+  },
+  {
+    "revision": "f4cbacb65d269e39d194",
+    "url": "/static/js/main.18aa2985.chunk.js"
+  },
+  {
+    "revision": "dfa312df2b27176ac920",
+    "url": "/static/js/runtime-main.e2ec6417.js"
+  },
+  {
+    "revision": "69e7f7dd98302d544fe33130bd0fd905",
+    "url": "/static/media/logo-small.69e7f7dd.png"
+  }
+]);
